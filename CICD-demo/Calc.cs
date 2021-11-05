ff7f0cfdfda75c88af941cb00a7e0f1030c0fee8
Emil Örjes <emil.orjes@hotmail.com>
2021-11-05 15:33:17 +0100

    Added calc class

---
+++ CICD-demo/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CICD_demo
+{
+    public class Calc
+    {
+        public int Add(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
