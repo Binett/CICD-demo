dcfcb65baefa4d8b09489f2dd272f2aa614e58f7
Binett <73723725+Binett@users.noreply.github.com>
2021-11-05 11:54:17 +0100

    Add test

---
+++ CICD-demo.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CICD_demo.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
