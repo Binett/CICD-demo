609d40f4021cfd6e756c777506a5afd7ffe8b162
Binett <73723725+Binett@users.noreply.github.com>
2021-11-08 15:46:48 +0100

    Changes Program.cs

--- CICD-demo/Program.cs
+++ CICD-demo/Program.cs
@@ -6,7 +6,7 @@ namespace CICD_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Emil!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
